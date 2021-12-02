7ca54ca200209a5720e82d2ea01475eef0438a16
achille2018 <a.tanwouo@alustudent.com>
2021-12-02 13:38:11 +0200

    aximal people for the elevator

--- people.c
+++ people.c
@@ -1,84 +1,41 @@
-// #include<stdio.h>
-// #include<stdlib.h>
-// #define maximal 10
-
-
-
-// int main()
-// {
-//     int size=0,i;
-//     int *array=malloc(sizeof(int)*maximal);
-//      if(!array) return 0;
-    
-
-//     for(i=0;i<maximal;i++)
-//  {
-//           array[i]=0;
-
-//     while (size<3)
-//     {
-//         printf("\n enter number \n :");
-//         scanf("%d",&array[i +i]);
-//         size+=1;
-
-//     } 
-    
-
-
-
-//  }
-//   if(size=10)
-//     {
-//         printf("the elevatore is full!");
-        
-        
-//     }
-
-//     else
-//     {
-//          printf("h");
-//     }
-    
-     
-     
-//           for(i=0;i<maximal;i++)
-//           {
-//           printf("\n%d\n",array[i]);
-//           }
-     
- 
-
-// }
-
-
-
 #include<stdio.h>
 #include<stdlib.h>
 
 int main()
 
-{
-    // int array[25],i,n=10;
+{ 
+    int i;
+    int maximum_people=10;
     int *array = malloc(sizeof(int) * 10);
 
     if (!array) return (0);
 
-    for (int j = 0; j < 10; j++ ) {
+    for (int j = 0; j < 10; j++ ) 
+    {
         array[j] = 0;
     }
-
-    for ( int i = 0; i < 10; i++)
+     
+    i =0; 
+    while (maximum_people)
     {
-      printf("inpuet floor number");
-      scanf("%d",array+i);
+        
+        
+        printf("Please enter your floor number \n:");
+        scanf("%d", array + i);
+        maximum_people--;
+        i++;
+        
         
     }
+    
 
     for ( int i = 0; i < 10; i++)
     {
-        printf("element %d :%d",i,*array+i);
+        printf("\nPersone %d :floor [[%d]] \n",i, *(array + i));
     
     }
     return 0;
     
 }
+
+
