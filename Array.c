3d56fba7c0cedea219bab2d5c04c36b95835943d
AdedejiArt <a.roheem@alustudent.com>
2021-12-02 11:07:07 -0800

    Added a file

---
+++ Array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ int i;
+
+ int Arr [10] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+int maximum_people = 10;
+while(maximum_people){
+    printf("Kindly enter your floor");
+    scanf("%d",&Arr[i] );
+    maximum_people=maximum_people - 1;
+    printf("%d",Arr[i]);
+
+
+
+
+
+
+
+
+ }
+
+
+
+
+
+
+}
